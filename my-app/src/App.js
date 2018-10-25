2803e36030ff4ca184b31eb91d362f345e26ba4b
qaasima <qaasima@uni.coventry.ac.ul>
2018-10-25 23:26:19 +0100

    adding the title

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
-
+import react_logo from './logo.svg';
 import './App.css';
 import Header from './components/header/Header'; 
 
 
 class App extends Component {
+  onSearch(term){
+
+    console.log("search on term:" + term);
+   }
   render() {
     return (
       <div>
-        <Header /> 
+        <Header title="My Own title" logo={react_logo} onSearchClick={this.onSearch} />
       </div>
     );
   }
