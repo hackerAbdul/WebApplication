2a2c74b11a6ba55e6172de51c7ca293dd7285d6e
qaasima <qaasima@uni.coventry.ac.ul>
2018-10-25 12:45:19 +0100

    adding the react header

---
+++ my-app/src/components/header/Header.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './Header.css';
+import react_logo from '../../logo.svg';
+
+class Header extends Component {
+
+    handleSearchSubmit(event){
+
+    //prevent the form to be submitted to its action url
+    event.preventDefault();
+    console.log("search button was clicked");
+    }
+
+    render() {
+        return (
+        <div className="header">
+            <img src={react_logo} alt="React logo" /><a href="#default" className="logo"> React App Header</a>
+            <div className="header-right">
+                <div className="search-container">
+                    <form action="">
+                        <input type="text" placeholder="Search.." name="txtSearch" onChange={this.handleInputChange} />
+                        <button type="submit" onClick={this.handleSearchSubmit}>Search</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        );
+    }
+}
+export default Header;
