8fc89305a2232f7c41e5caeeefee635ed76305dc
qaasima <qaasima@uni.coventry.ac.ul>
2018-10-28 19:32:13 +0000

    new website with addition of footer and navigation bar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,17 +2,24 @@ import React, { Component } from 'react';
 import react_logo from './logo.svg';
 import './App.css';
 import Header from './components/header/Header'; 
+import Categories from './components/categories/Categories';
+import Footer from './components/Footer/Footer';
 
 
 class App extends Component {
-  onSearch(term){
 
-    console.log("search on term:" + term);
-   }
   render() {
     return (
-      <div>
-        <Header title="My Own title" logo={react_logo} onSearchClick={this.onSearch} />
+      <div className="container">
+        <div className="header">
+          <Header logo={react_logo} />
+        </div>
+        <div className="categories">
+          <Categories />
+        </div>
+        <div>
+          <Footer />
+        </div>    
       </div>
     );
   }
