5641f182f1a153c57e9190443e5ee5248d8d5618
qaasima <qaasima@uni.coventry.ac.ul>
2018-10-30 00:01:05 +0000

    fully finished the footer does not contain routes for links

--- my-app/src/components/Footer/Footer.js
+++ my-app/src/components/Footer/Footer.js
@@ -4,8 +4,33 @@ import './Footer.css';
 class Footer extends Component{
   render(){
     return (
-      <div className="footer">
-        <p>Footer</p>
+      <div>
+        <div className="footer">
+          <p>Footer</p>
+          <table id="Content">
+          <tbody>
+            <tr>
+              <th>About</th>
+              <th>Help/Contact</th>
+              <th>Social Media</th>
+            </tr>
+            <tr>
+              <td><a href="link1">About Us</a></td>
+              <td><a href="link2">FAQs</a></td>
+              <td><a href="link3">FaceBook</a></td>
+            </tr>
+            <tr>
+              <td><a href="link4">Terms & Contditions</a></td>
+              <td><a href="link5">Contact Us</a></td>
+              <td><a href="link6">Twitter</a></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td><a href="link7">Email</a></td>
+              <td></td>
+            </tr>
+          </tbody></table>
+        </div>
       </div>
     );
   };
