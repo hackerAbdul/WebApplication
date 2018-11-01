926bc8e610ae594ed6a5cf61ca5be0b4e346e4c0
qaasima <qaasima@uni.coventry.ac.uk>
2018-11-01 14:43:56 +0000

    starting to make the body to the page

--- my-app/src/components/body/Body.js
+++ my-app/src/components/body/Body.js
@@ -1,13 +1,16 @@
+import React, { Component } from 'react';
+import './Body.css';
 
 
-var NewComponent = React.createClass({
-    render: function() {
-      return (
-        <div>
-          <h1>Hello world!</h1>
-          <p><a href="https://www.w3schools.com">Visit W3Schools.com!</a></p>
-          <p>The bgcolor attribute is not supported in HTML5. Use CSS instead.</p>
-        </div>
-      );
-    }
-  });
+class Body extends Component{
+  render(){
+    return (
+      <div>
+        <h1>Hello world!</h1>
+        <p><a href="https://www.w3schools.com">Visit W3Schools.com!</a></p>
+        <p>The bgcolor attribute is not supported in HTML5. Use CSS instead.</p>
+      </div>
+    );
+  };
+};
+export default Body;
