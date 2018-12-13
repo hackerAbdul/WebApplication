211a325970a0c5e9360232f1d7a1d9f99ba5ac80
qaasima <qaasima@uni.coventry.ac.ul>
2018-12-13 17:41:12 +0000

    added patch and delete to products

--- API/Routes/Products.js
+++ API/Routes/Products.js
@@ -5,9 +5,24 @@ const mongoose = require('mongoose')
 const Product = require('../models/products');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: "Handles GET request for /products"
-    });
+    Product.find()
+    .exec()
+    .then(docs => {
+        console.log(docs);
+        if (docs.length <= 0){
+            res.status(404).json({
+                message: "No users found"
+            });
+        }else{
+            res.status(200).json(docs)
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    })
 });
 
 router.post('/', (req, res, next) => {
@@ -62,17 +77,45 @@ router.get('/:productId', (req, res, next) => {
 
 //PATCH route
 router.patch('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: "Updated Product"
+    const id = req.params.productId;
+    const updateOps = {};
+    for (const ops of req.body){
+        updateOps[ops.propName] = ops.value;
+    }
+    Product.update({
+        _id: id
+    },
+    {$set: updateOps})
+    .exec()
+    .then(result => {
+        console.log(result);
+        res.status(200).json({
+            message: "Updated"
+        })
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
     });
 });
 
 
 //Delete route
 router.delete('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: "Deleted Product"
-    });
+    const id = req.params.productId
+    Product.deleteOne({
+        _id: id
+    })
+    .exec()
+    .then(result => {
+        res.status(200).json(result);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error : err});
+    })
 });
 
 
