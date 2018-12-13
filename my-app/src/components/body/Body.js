4c3fcdc68b6700f5ecf5f2a0744720aa7e1eb9cf
qaasima <qaasima@uni.coventry.ac.ul>
2018-12-13 20:26:00 +0000

    adding grid into fron page

--- my-app/src/components/body/Body.js
+++ my-app/src/components/body/Body.js
@@ -6,37 +6,7 @@ class Body extends Component{
   render(){
     return (
       <div>
-        <div>
-          <h1>Hello world!</h1>
-          <p><a href="https://www.w3schools.com">Visit W3Schools.com!</a></p>
-          <p>The bgcolor attribute is not supported in HTML5. Use CSS instead.</p>
-        </div>
-        <div>
-        <h1>Hello world!</h1>
-        <p><a href="https://www.w3schools.com">Visit W3Schools.com!</a></p>
-        <p>The bgcolor attribute is not supported in HTML5. Use CSS instead.</p>
       </div>
-      <div>
-        <h1>Hello world!</h1>
-        <p><a href="https://www.w3schools.com">Visit W3Schools.com!</a></p>
-        <p>The bgcolor attribute is not supported in HTML5. Use CSS instead.</p>
-      </div>
-      <div>
-        <h1>Hello world!</h1>
-        <p><a href="https://www.w3schools.com">Visit W3Schools.com!</a></p>
-        <p>The bgcolor attribute is not supported in HTML5. Use CSS instead.</p>
-      </div>
-      <div>
-        <h1>Hello world!</h1>
-        <p><a href="https://www.w3schools.com">Visit W3Schools.com!</a></p>
-        <p>The bgcolor attribute is not supported in HTML5. Use CSS instead.</p>
-      </div>
-      <div>
-        <h1>Hello world!</h1>
-        <p><a href="https://www.w3schools.com">Visit W3Schools.com!</a></p>
-        <p>The bgcolor attribute is not supported in HTML5. Use CSS instead.</p>
-      </div>
-    </div>
       
     );
   };
--- my-app/src/pages/HomePage.js
+++ my-app/src/pages/HomePage.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import '../App.css';
 import Body from '../components/body/Body';
+import Grid from './../components/grid/Grid';
+import Data from '../Data';
 
 /*this is the homepage only displays the body on here footer and header is 
 always printed as a continuous function call*/
@@ -13,6 +15,7 @@ class HomePage extends Component {
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <div>
             <Body />
+            <Grid items={Data.items} colClass="col-m-3" onClick={this.handleThumbnailClicked} rowLength={4} />
           </div> 
         </div>
       );
